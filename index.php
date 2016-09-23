c312e3d081d0f69476cea1e022a5af2b50f06001
HankNguyen <t.hung.95@gmail.com>
2016-09-23 17:08:48 +0800

    init commit

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/libs/Facebook/autoload.php';
 require_once __DIR__ . '/app_config.php';
 
 $fb = new Facebook\Facebook([
-  'app_id' => 'APP_ID',
-  'app_secret' => 'APP_SECRET',
+  'app_id' => APP_ID,
+  'app_secret' => APP_SECRET,
   'default_graph_version' => 'v2.4',
   ]);
 
