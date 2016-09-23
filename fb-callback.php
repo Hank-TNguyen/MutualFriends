fa05de7cb60557641c641f1667a15b52bb4c63a3
HankNguyen <t.hung.95@gmail.com>
2016-09-23 18:01:55 +0800

    init commit

--- fb-callback.php
+++ fb-callback.php
@@ -9,23 +9,9 @@ $fb = new Facebook\Facebook([
   'default_graph_version' => 'v2.7',
   ]);
 
-$helper = $fb->getRedirectLoginHelper();
+// $helper = $fb->getRedirectLoginHelper();
 
-try {
-  // Get the \Facebook\GraphNodes\GraphUser object for the current user.
-  // If you provided a 'default_access_token', the '{access-token}' is optional.
-  $response = $fb->get('/me', '{access-token}');
-} catch(\Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(\Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
 
-$me = $response->getGraphUser();
-echo 'Logged in as ' . $me->getName();
+echo 'Logged in as ';
 
 ?>
