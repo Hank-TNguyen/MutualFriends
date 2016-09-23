2f14f96a97c460892ae5d481b83f09c7e181312f
HankNguyen <t.hung.95@gmail.com>
2016-09-23 17:50:54 +0800

    init commit

--- fb-callback.php
+++ fb-callback.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/libs/Facebook/autoload.php';
 require_once __DIR__ . '/app_config.php';
 
 $fb = new Facebook\Facebook([
-  'app_id' => '{app-id}', // Replace {app-id} with your app id
-  'app_secret' => '{app-secret}',
-  'default_graph_version' => 'v2.2',
+  'app_id' => APP_ID, // Replace {app-id} with your app id
+  'app_secret' => APP_SECRET,
+  'default_graph_version' => 'v2.7',
   ]);
 
 $helper = $fb->getRedirectLoginHelper();
